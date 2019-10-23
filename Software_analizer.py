0420183492da748ccc5c6011cb41a2b1102eb5b0
davenisc <jodaveza@gmail.com>
2019-10-22 21:15:51 -0500

    Add files via upload

--- Software_analizer.py
+++ Software_analizer.py
@@ -67,7 +67,7 @@ print(Fore.RED + "          Software link "+Fore.GREEN+" : https://github.com/da
 init()
 print(Fore.RED + "          Description "+Fore.GREEN+"   : SCRIPT FOR RAPID SOFTWARE ANALYSIS")
 init()
-print(Fore.RED + "          Contact Mail "+Fore.GREEN+"  : sysvd@protonmail.com")
+print(Fore.RED + "          Contact Mail "+Fore.GREEN+"  : info@davenisc.com")
 
 #Color de texto inicio ###################################################################
 
@@ -102,7 +102,7 @@ print(Fore.RED + "                 | @DaveNISC |              |  @whitepadawan |
 init()
 print(Fore.RED + "                 +-------------------------------------------+              ")
 init()
-print (Fore.GREEN +"                            By SySvd  | v 1.0.0                           ")
+print (Fore.GREEN +"                            By davenisc.com  | v 1.2.0                    ")
 init()
 print (Fore.WHITE +"________________________________________________________________________\n")
  
@@ -182,6 +182,9 @@ while not salir:
 
         #os.system(command)
 
+
+        ###################################################################################### buscando MD5
+
         command = '''  md5sum  ''' + soft_a +  ''' |awk '{print $1}' > log_hash.txt '''
 
         os.system(command)
@@ -210,11 +213,80 @@ while not salir:
 
         var = os.getcwd()
 
+        ###################################################################################### buscando sha256
+
+        command = '''  sha256sum  ''' + soft_a +  ''' |awk '{print $1}' > log_sha256.txt '''
+
+        os.system(command)
+
+        search_hash256 = open('hash256.txt','w')
+
+        regex = '[A-Za-z-0-9]{64,64}'
+
+
+        datos3 = open('log_sha256.txt')
+        for line3 in datos3:
+            line3 = line3.rstrip()
+            sha256 = re.findall('[[A-Za-z-0-9]{64,64}', line3)
+            if len(sha256) > 0 :
+                search_hash256.write(str(sha256[0]+ "\n"))
+                #print (y[0])
+
+        search_hash256 = open('hash256.txt','w')
+
+
+        lines = [line.strip() for line in open('log_sha256.txt', 'r')]
+        for b in lines:
+            found_h = re.search('[[A-Za-z-0-9]{64,64}', b)
+            if found_h:
+                search_hash256.write(b + "\n")
+
+        var = os.getcwd()
+
+        ###################################################################################### buscando sha1
+
+        command = '''  sha1sum  ''' + soft_a +  ''' |awk '{print $1}' > log_sha1.txt '''
+
+        os.system(command)
+
+        search_hash1 = open('hash1.txt','w')
+
+        regex = '[A-Za-z-0-9]{40,40}'
+
+
+        datos4 = open('log_sha1.txt')
+        for line4 in datos4:
+            line4 = line4.rstrip()
+            sha1 = re.findall('[[A-Za-z-0-9]{40,40}', line4)
+            if len(sha1) > 0 :
+                search_hash1.write(str(sha1[0]+ "\n"))
+                #print (y[0])
+
+        search_hash1 = open('hash1.txt','w')
+
+
+        lines = [line.strip() for line in open('log_sha1.txt', 'r')]
+        for c in lines:
+            found_h = re.search('[[A-Za-z-0-9]{40,40}', c)
+            if found_h:
+                search_hash1.write(c + "\n")
+
+        var = os.getcwd()
+
+
+        #####################################################################################################
+
+
         command = '  automater  ' + var+'/hash.txt' +  ' > log_scan.txt '
         os.system(command)
 
-        command = '  exiftool  ' + soft_a +  ' > log_metadata.txt '
+        command = '  file  ' + soft_a +  ' > log_format.txt '
+        os.system(command)
 
+        command = '  string  ' + soft_a +  ' > log_strings.txt '
+        os.system(command)
+
+        command = '  exiftool  ' + soft_a +  ' > log_metadata.txt '
         os.system(command)
     
         for i in tqdm(range(100)):
@@ -229,18 +301,20 @@ while not salir:
         
     elif opcion == 2:
 
-        ############################################################################################################
+        ######################################################################################## mostrando hash MD5
         print ("")    
 
         init()
         print (Fore.WHITE +"________________________________________________________________________\n")
         
-        print(Fore.RED + "[*]"+Fore.WHITE+" Hash found (MD5)\n")
+        print(Fore.RED + "[*]"+Fore.GREEN+" Hash found (MD5)\n")
+
+        init()
+        print (Fore.WHITE +"")
 
         print_hash = open('hash.txt','r')
 
-        #[for].[0-9]+.[0-9]+.[0-9]+.[0-9]+ IP del server
-        #[0-9].+[a-z]\s*[a-z]+\s\s[a-z]+ puerto 
+    
 
         regex = '[A-Za-z-0-9]{32,32}'
 
@@ -253,19 +327,77 @@ while not salir:
                 if y[0] not in list_hash:
                     list_hash.append(y[0])
                     print (y[0])
-                    
+
+        ############################################################################## mostrando sha256
+
+        print ("") 
+
+        print(Fore.RED + "[*]"+Fore.GREEN+" Hash found (sha256)\n")
+
         init()
-        print (Fore.WHITE +"________________________________________________________________________\n")
+        print (Fore.WHITE +"")
+
+        print_hash = open('hash256.txt','r')
+
+    
+
+        regex = '[A-Za-z-0-9]{64,64}'
+
+        list_hash = []
+
+        for line3 in print_hash:
+            line3 = line3.rstrip()
+            x = re.findall('[A-Za-z-0-9]{64,64}', line3)
+            if len(x) > 0 :
+                if x[0] not in list_hash:
+                    list_hash.append(x[0])
+                    print (x[0])
+
+
+
+        ############################################################################## mostrando sha256
+
+        print ("") 
+
+        print(Fore.RED + "[*]"+Fore.GREEN+" Hash found (sha1)\n")
+
+        init()
+        print (Fore.WHITE +"")
+
+        print_hash = open('hash1.txt','r')
+
+    
+
+        regex = '[A-Za-z-0-9]{40,40}'
+
+        list_hash = []
+
+        for line4 in print_hash:
+            line4 = line4.rstrip()
+            z = re.findall('[A-Za-z-0-9]{40,40}', line4)
+            if len(z) > 0 :
+                if z[0] not in list_hash:
+                    list_hash.append(z[0])
+                    print (z[0])
+
+
+
+                    
+        #init()
+        #print (Fore.WHITE +"________________________________________________________________________\n")
         time.sleep(3)
         print ("")
-        ########################################################################################
+        ######################################################################################## mostrando metadata
 
         print ("")    
 
         init()
         print (Fore.WHITE +"________________________________________________________________________\n")
         
-        print(Fore.RED + "[*]"+Fore.WHITE+" Data found (Metadata)\n")
+        print(Fore.RED + "[*]"+Fore.GREEN+" Data found (Metadata)\n")
+
+        init()
+        print (Fore.WHITE +"")
 
         print_data = open('log_metadata.txt','r')
 
@@ -283,26 +415,85 @@ while not salir:
                 if b[0] not in list_data:
                     list_data.append(b[0])
                     print (b[0])
-        init()
-        print (Fore.WHITE +"________________________________________________________________________\n")
+        #init()
+        #print (Fore.WHITE +"________________________________________________________________________\n")
         time.sleep(3)
         print ("")
         
         #######################################################################################################
+
+
+        ######################################################################################## mostrando formato y tipo
 
         print ("")    
 
         init()
         print (Fore.WHITE +"________________________________________________________________________\n")
         
-        print(Fore.RED + "[*]"+Fore.WHITE+"  Malware analysis result (AV)\n")
+        print(Fore.RED + "[*]"+Fore.GREEN+" file type and format found (type/format)\n")
+
+        init()
+        print (Fore.WHITE +"")
+
+        print_data = open('log_format.txt','r')
+
+        regex = '([A].+)\w+'
+
+        list_data = []
+
+        for line14 in print_data:
+            line14 = line14.rstrip()
+            b = re.findall('([A].+)\w+', line14)
+            if len(b) > 0 :
+                if b[0] not in list_data:
+                    list_data.append(b[0])
+                    print (b[0])
+        #init()
+        #print (Fore.WHITE +"________________________________________________________________________\n")
+        time.sleep(3)
+        print ("")
+        
+
+        ######################################################################################## mostrando strings de binario
+
+        print ("")    
+
+        init()
+        print (Fore.WHITE +"________________________________________________________________________\n")
+        
+        print(Fore.RED + "[*]"+Fore.RED+" String results found (IP/URL/DLL) this function will be added soon \n")
+
+        print_data = open('log_strings.txt','r')
+
+        regex = '[\w].+'
+
+        list_data = []
+
+        for line3 in print_data:
+            line3 = line3.rstrip()
+            b = re.findall('[\w].+', line3)
+            if len(b) > 0 :
+                if b[0] not in list_data:
+                    list_data.append(b[0])
+                    print (b[0])
+        #init()
+        #print (Fore.WHITE +"________________________________________________________________________\n")
+        time.sleep(3)
+        print ("")
+        ########################################################################################### mostrando resultados antivirus
+
+        print ("")    
+
+        init()
+        print (Fore.WHITE +"________________________________________________________________________\n")
+        
+        print(Fore.RED + "[*]"+Fore.GREEN+"  Malware analysis result (AV)\n")
+
+        init()
+        print (Fore.WHITE +"")
         
         print_data = open('log_scan.txt','r')
 
-        #[for].[0-9]+.[0-9]+.[0-9]+.[0-9]+ IP del server
-        #[0-9].+[a-z]\s*[a-z]+\s\s[a-z]+ puerto 
-        init()
-        print (Fore.WHITE +"")
         regex = '([BitDefender]{11,11}....+)'
 
         list_data = []
@@ -489,10 +680,258 @@ while not salir:
                     #init()
                     #print (Fore.RED +p[0])
                     t = PrettyTable(['(*)', p])
-                    print t                        
+                    print t  
 
-        init()
-        print (Fore.WHITE +"________________________________________________________________________\n")
+
+        print_data20 = open('log_scan.txt','r')
+
+        regex = '([FireEye]{7,7}....+)'
+
+        list_data20 = []
+
+        for line20 in print_data20:
+            line20 = line20.rstrip()
+            q = re.findall('([FireEye]{7,7}....+)', line20)
+            if len(q) > 0 :
+                if q[0] not in list_data20:
+                    list_data20.append(q[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', q])
+                    print t    
+
+
+        print_data30 = open('log_scan.txt','r')
+
+        regex = '([AegisLab]{8,8}....+)'
+
+        list_data30 = []
+
+        for line30 in print_data30:
+            line30 = line30.rstrip()
+            r = re.findall('([AegisLab]{8,8}....+)', line30)
+            if len(r) > 0 :
+                if r[0] not in list_data30:
+                    list_data30.append(r[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', r])
+                    print t       
+
+
+
+        print_data32 = open('log_scan.txt','r')
+
+        regex = '([ALYac]{5,5}....+)'
+
+        list_data32 = []
+
+        for line32 in print_data32:
+            line32 = line32.rstrip()
+            w = re.findall('([ALYac]{5,5}....+)', line32)
+            if len(w) > 0 :
+                if w[0] not in list_data32:
+                    list_data32.append(w[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', w])
+                    print t         
+
+
+        print_data29 = open('log_scan.txt','r')
+
+        regex = '([Arcabit]{7,7}....+)'
+
+        list_data29 = []
+
+        for line29 in print_data29:
+            line29 = line29.rstrip()
+            o = re.findall('([Arcabit]{7,7}....+)', line29)
+            if len(o) > 0 :
+                if o[0] not in list_data29:
+                    list_data29.append(o[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', o])
+                    print t         
+                         
+
+        print_data28 = open('log_scan.txt','r')
+
+        regex = '([Cyren]{5,5}....+)'
+
+        list_data28 = []
+
+        for line28 in print_data28:
+            line28 = line28.rstrip()
+            n = re.findall('([Cyren]{5,5}....+)', line28)
+            if len(n) > 0 :
+                if n[0] not in list_data28:
+                    list_data28.append(n[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', n])
+                    print t      
+
+
+
+
+        print_data27 = open('log_scan.txt','r')
+
+        regex = '([DrWeb]{5,5}....+)'
+
+        list_data27 = []
+
+        for line27 in print_data27:
+            line27 = line27.rstrip()
+            e = re.findall('([DrWeb]{5,5}....+)', line27)
+            if len(e) > 0 :
+                if e[0] not in list_data27:
+                    list_data27.append(e[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', e])
+                    print t    
+
+
+
+        print_data26 = open('log_scan.txt','r')
+
+        regex = '([F-Secure]{8,8}....+)'
+
+        list_data26 = []
+
+        for line26 in print_data26:
+            line26 = line26.rstrip()
+            a = re.findall('([F-Secure]{8,8}....+)', line26)
+            if len(a) > 0 :
+                if a[0] not in list_data26:
+                    list_data26.append(a[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', a])
+                    print t      
+
+
+
+        print_data25 = open('log_scan.txt','r')
+
+        regex = '([Comodo]{6,6}....+)'
+
+        list_data25 = []
+
+        for line25 in print_data25:
+            line25 = line25.rstrip()
+            i = re.findall('([Comodo]{6,6}....+)', line25)
+            if len(i) > 0 :
+                if i[0] not in list_data25:
+                    list_data25.append(i[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', i])
+                    print t      
+
+
+        print_data24 = open('log_scan.txt','r')
+
+        regex = '([Emsisoft]{8,8}....+)'
+
+        list_data24 = []
+
+        for line24 in print_data24:
+            line24 = line24.rstrip()
+            z = re.findall('([Emsisoft]{8,8}....+)', line24)
+            if len(z) > 0 :
+                if z[0] not in list_data24:
+                    list_data24.append(z[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', z])
+                    print t      
+
+
+
+
+        print_data22 = open('log_scan.txt','r')
+
+        regex = 'ESET-NOD32....+'
+
+        list_data22 = []
+
+        for line22 in print_data22:
+            line22 = line22.rstrip()
+            uu = re.findall('ESET-NOD32....+', line22)
+            if len(uu) > 0 :
+                if uu[0] not in list_data22:
+                    list_data22.append(uu[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', uu])
+                    print t    
+
+
+        print_data23 = open('log_scan.txt','r')
+
+        regex = 'Ad-Aware....+'
+
+        list_data23 = []
+
+        for line23 in print_data23:
+            line23 = line23.rstrip()
+            x = re.findall('Ad-Aware....+', line23)
+            if len(x) > 0 :
+                if x[0] not in list_data23:
+                    list_data23.append(x[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', x])
+                    print t    
+
+
+
+        print_data31 = open('log_scan.txt','r')
+
+        regex = 'AhnLab-V3....+'
+
+        list_data31 = []
+
+        for line31 in print_data31:
+            line31 = line31.rstrip()
+            v = re.findall('AhnLab-V3....+', line31)
+            if len(v) > 0 :
+               if v[0] not in list_data31:
+                   list_data31.append(v[0])
+                   #init()
+                   #print (Fore.RED +p[0])
+                   t = PrettyTable(['(*)', v])
+                   print t    
+
+
+
+        print_data21 = open('log_scan.txt','r')
+
+        regex = 'CAT-QuickHeal....+'
+
+        list_data21 = []
+
+        for line21 in print_data21:
+            line21 = line21.rstrip()
+            sa = re.findall('CAT-QuickHeal....+', line21)
+            if len(sa) > 0 :
+                if sa[0] not in list_data21:
+                    list_data21.append(sa[0])
+                    #init()
+                    #print (Fore.RED +p[0])
+                    t = PrettyTable(['(*)', sa])
+                    print t              
+
+           
+
+
+
+
+        #init()
+        #print (Fore.WHITE +"________________________________________________________________________\n")
         time.sleep(3)
         print ("")
 
@@ -517,6 +956,10 @@ while not salir:
         print(Fore.CYAN + "[1]"+Fore.WHITE+"       Md5sum V 8.28")
         init()
         print(Fore.CYAN + "[2]"+Fore.WHITE+"       Automater ")
+        init()
+        print(Fore.CYAN + "[2]"+Fore.WHITE+"       File type ")
+        init()
+        print(Fore.CYAN + "[2]"+Fore.WHITE+"       String ")
         init()
         print(Fore.CYAN + "[3]"+Fore.WHITE+"       Exiftool V 10.80\n")
         init()
